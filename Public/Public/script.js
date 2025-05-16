845a59bb098ebb01dd3c4850b606b817a7dd8a8d
AnnMariya17 <annmariyakg73@gmail.com>
2025-05-16 16:32:44 +0530

    Add files via upload

---
+++ Public/Public/script.js
@@ -0,0 +1,15 @@
+
+    var a;
+    var b;
+    function addNumbers(a,b){
+        return a+b;
+    }
+        
+    document.getElementById("msg").innerHTML=addNumbers(5,10);
+
+    function show(){
+        window.alert("YOU HAVE COMPLETED THE FORM");
+    }
+
+   
+    
